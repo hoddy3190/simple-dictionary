53247f350c1d92bddf31421db047a7861e8593f7
Hodaka Suzuki <altitude3190@gmail.com>
2020-07-30 15:33:23 +0900

    refactor: delete deactivate() because it's unused

--- src/extension.ts
+++ src/extension.ts
@@ -28,6 +28,3 @@ export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(disposable);
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
